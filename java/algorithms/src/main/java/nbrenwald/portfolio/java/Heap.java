2c72130e4bc69348880cabadaf7f975dec17503b
nbrenwald <nbrenwald@gmail.com>
2014-04-28 22:50:11 +0200

    heapify tested and working

--- java/algorithms/src/main/java/nbrenwald/portfolio/java/Heap.java
+++ java/algorithms/src/main/java/nbrenwald/portfolio/java/Heap.java
@@ -1,102 +1,97 @@
 package nbrenwald.portfolio.java;
 
 public class Heap {
-  // Parent  = i-1/2
+  // Parent = i-1/2
   // left child = i*2+1
   // right child = i*2+2
-  
+
   private int[] array;
   private int endPointer;
-  
-  private Heap(){
-  }
-  
-  
+
+  private Heap() {}
+
+
   private Heap(int[] inArray, int i) {
     array = inArray;
     endPointer = i;
-    
-    
   }
 
 
-  public static Heap emptyHeap(){
+  public static Heap emptyHeap() {
     return new Heap();
   }
-  
-  private static void heapify(int[] inArray, int endPointer){
-    System.out.println(endPointer);
-    print(inArray);
-    // start with right most subtree. 
-    for(int i = (endPointer-1/2); i>=0; i--){
-      // call siftdown
-      int currentNode = i;
-      int leftChild = 2*i+1;
-      int rightChild = 2*i+1;
-      while(leftChild <= endPointer){
-        if(inArray[currentNode]<inArray[leftChild]){
-          // swap current with left, then set current to be the left child
-          int tmp = inArray[leftChild];
-          inArray[leftChild]=inArray[currentNode];
-          inArray[currentNode]=tmp;
-          currentNode = leftChild;
-          leftChild = 2*currentNode+1;
-          rightChild = 2*currentNode+2;
-        }
-        else if (rightChild <=endPointer && inArray[currentNode]<inArray[rightChild]){
-       // swap current with left, then set current to be the left child
-          int tmp = inArray[rightChild];
-          inArray[rightChild]=inArray[currentNode];
-          inArray[currentNode]=tmp;
-          currentNode = leftChild;
-          leftChild = 2*currentNode+1;
-          rightChild = 2*currentNode+2;
-        }
-        else {
-          break;
-        }
-      }
+
+  private static void siftDown(int[] inArray, int node, int end) {
+    // Check the left child and right child, swap the current node with the the largest child 
+    // which is greater than the current node.
+    int leftChild = (2 * node) + 1;
+    int rightChild = (2 * node) + 2;
+    int largest = node;
+    if (leftChild <= end && inArray[leftChild] > inArray[node]) {
+      largest = leftChild;
+    }
+
+    if (rightChild <= end && inArray[rightChild] > inArray[largest]) {
+      largest = rightChild;
+    }
+
+    if (largest != node) {
+      int tmp = inArray[node];
+      inArray[node] = inArray[largest];
+      inArray[largest] = tmp;
+      siftDown(inArray, largest, end);
     }
   }
 
-  
-  
-  public static Heap fromArray(int[] inArray){
+  private static void heapify(int[] inArray, int endPointer) {
+    // Starting with the right most parent node. Call siftDown on all higher nodes.
+    // This is order O(n)
+    for (int i = ((endPointer - 1) / 2); i >= 0; i--) {
+      siftDown(inArray, i, endPointer);
+
+    }
+  }
+
+
+
+  public static Heap fromArray(int[] inArray) {
     // Static Factory to return a heap
     // Lets clone the array given to us, allowing twice the length for growth
-    int[] newArray = new int[2*inArray.length];
-    int newEndPointer = inArray.length-1;
-    
-    for(int i = 0 ; i<inArray.length; i++){
+    int[] newArray = new int[2 * inArray.length];
+    int newEndPointer = inArray.length - 1;
+
+    for (int i = 0; i < inArray.length; i++) {
       newArray[i] = inArray[i];
     }
-    Heap.heapify(newArray,newEndPointer);
-    
-    Heap newHeap = new Heap(newArray,newEndPointer);
+    Heap.heapify(newArray, newEndPointer);
+    Heap newHeap = new Heap(newArray, newEndPointer);
     return newHeap;
-    
+
   }
-  
-  
-  public int max(){
+
+
+  public int max() {
     return array[0];
   }
-  
-  public boolean checkRI(){
+
+  public boolean checkRI() {
     // Check the representation invariant.
     // Start with the leaves, check that all parents are large, then check parents etc.
     int parentIndex;
-    for(int i = endPointer; i>0; i--){
-      parentIndex=(endPointer-1)/2;
-      if(array[parentIndex] < array[i]) return false;
+    for (int i = endPointer; i > 0; i--) {
+      parentIndex = (i - 1) / 2;
+      if (array[parentIndex] < array[i]) {
+        System.out.println("Parent Index = " + parentIndex + " index = " + i);
+        return false;
+      }
     }
     return true;
-    
+
   }
-  
-  private static void print(int[] inArray){
-    for(int i : inArray){
-      System.out.print(i+", ");
+
+  private static void print(int[] inArray) {
+    for (int i : inArray) {
+      System.out.print(i + ", ");
     }
     System.out.println();
   }
